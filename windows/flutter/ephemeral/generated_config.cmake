d88d4d5ae92305f68800f582597329b55e832a57
rudwnsJun <tjsql07@gmail.com>
2024-12-04 00:27:35 +0900

    Deploy Flutter web app to GitHub Pages

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter files\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\user\\OneDrive\\바탕 화면\\page1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter files\\flutter"
+  "PROJECT_DIR=C:\\Users\\user\\OneDrive\\바탕 화면\\page1"
+  "FLUTTER_ROOT=C:\\flutter files\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\user\\OneDrive\\바탕 화면\\page1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\user\\OneDrive\\바탕 화면\\page1"
+  "FLUTTER_TARGET=C:\\Users\\user\\OneDrive\\바탕 화면\\page1\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\user\\OneDrive\\바탕 화면\\page1\\.dart_tool\\package_config.json"
+)
